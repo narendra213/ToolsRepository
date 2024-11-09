7f33907a846d436d5c8e99a71e707c278e7ce2a2
narendra213 <narendranew213>
2024-11-09 15:55:31 +0530

    commited by TL

--- GitProj01/src/main/java/com/nit/main/App.java
+++ GitProj01/src/main/java/com/nit/main/App.java
@@ -7,6 +7,7 @@ public class App {
     public static void main(String[] args) {
         App app=new App();
         System.out.println(" TL....the sum is::"+app.sum(10, 20));
+        //hello
         
     }
 }
